21c2e58d019f2bb88fb86684a8f4b68d45659660
uruha <kochmusica@gmail.com>
2020-06-13 15:05:57 +0900

    update: add plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,6 +87,7 @@ module.exports = {
         policy: [{ userAgent: `*`, allow: `/` }],
       },
     },
+    `gatsby-plugin-react-helmet`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
