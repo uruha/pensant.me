88857840163264cc7e3bb9586326b34a2da02c7c
uruha <kochmusica@gmail.com>
2020-06-13 14:50:34 +0900

    update: add robot.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,8 +79,16 @@ module.exports = {
         icon: `content/assets/icon.png`,
       },
     },
+    {
+      resolve: `gatsby-plugin-robots-txt`,
+      options: {
+        host: `https://pensant.me.com`,
+        sitemap: `https://pensant.me.com/sitemap.xml`,
+        policy: [{ userAgent: `*`, allow: `/` }],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-};
+}
