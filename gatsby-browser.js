72a8cd6e5613bf7884ebf199713f3a2f7b60aece
uruha <kochmusica@gmail.com>
2020-06-13 13:00:26 +0900

    update: change global css

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
-// node modules
-import "normalize.css/normalize.css"
-import "prismjs/themes/prism.css"
+// // node modules
+// import "normalize.css/normalize.css"
+// import "prismjs/themes/prism.css"
 
-// custom
-import "./src/styles/global.css"
+// // custom
+// import "./src/styles/global.css"
